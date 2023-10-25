a9e50d1165e98a45104cda2947d6da51a90f2d17
NicholasZHeaton <Nicholaszheaton@gmail.com>
2023-10-25 08:53:03 -0400

    Added some comments before som loops and if statements. Added an if if statement to world championship to check for instances where both players reach 100 or more. Changed all > to >= in world championship or any other needed spots.

--- PigGame.py
+++ PigGame.py
@@ -15,7 +15,6 @@
 import random
 
 
-# roll_dice() provides two separate dice, rolling random numbers between 1 and 6 whenever the function is called on.
 def roll_dice():
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
@@ -24,10 +23,6 @@ def roll_dice():
 
 def computer_turn(game_score, goal):
     turn_total = 0
-
-    # while the turn_total is less than the goal total then the loop will continue until you roll both dice as 1's
-    # setting game_score to 0. If that passes then it checks for a single occurrence of 1 in the dice, setting the
-    # current score to 0. If no 1's are present then the dice totals are added up and printed.
     while turn_total < goal:
         computer_roll = roll_dice()
         if computer_roll.count(1) == 2:
@@ -63,6 +58,8 @@ def human_turn(game_score):
 
 def validate_response(prompt, response_1, response_2):
     prompt = input(prompt).lower()
+
+    # checks if the given prompt is either response 1 or 2 and asks you to correct your answer otherwise.
     while prompt != response_1 and prompt != response_2:
         print(f'Response not valid, try {response_1} or {response_2}.')
         prompt = input().lower()
@@ -72,6 +69,8 @@ def validate_response(prompt, response_1, response_2):
 def human_vs_computer():
     human_score = 0
     computer_score = 0
+
+    # The game will continue between a player and computer until one of them reaches 100 points or more.
     while human_score < 100 and computer_score < 100:
         human_score = human_turn(human_score)
         computer_score = computer_turn(computer_score, 20)
@@ -85,11 +84,13 @@ def human_vs_computer():
 def computer_solo(goal):
     score = 0
     turns = 0
+
+    # A computer plays the dice game alone until it reaches 100 points, counting how many turns it takes to reach 100.
     while score < 100:
         score = computer_turn(score, goal)
         turns += 1
         print('Turn:', turns, 'Score:', score)
-        if score > 100:
+        if score >= 100:
             print('Turns:', turns)
             return turns
 
@@ -100,15 +101,26 @@ def world_championship(games, goal_1, goal_2):
     swear_im_human = 0
     totes_not_robot_wins = 0
     swear_im_human_wins = 0
+
+    # a game between two computers that loops until a given number of games has been finished. the winner is whichever
+    # computer has most games won by the end of all games played.
     while game_total != 0:
-        if totes_not_robot > 100:
+
+        # an if if statement provided to give points to whichever computer has more points in the case where they both
+        # reach 100 or more points in a game.
+        if totes_not_robot >= 100 and swear_im_human >= 100:
+            if totes_not_robot > swear_im_human:
+                totes_not_robot_wins += 1
+            else:
+                swear_im_human_wins += 1
+        elif totes_not_robot >= 100:
             totes_not_robot_wins += 1
-            game_total -= 1
-            print("totes_not_robot:", totes_not_robot, "swear_im_human:", swear_im_human)
-            swear_im_human -= swear_im_human
-            totes_not_robot -= totes_not_robot
-        elif swear_im_human > 100:
+        elif swear_im_human >= 100:
             swear_im_human_wins += 1
+
+        # when only one reaches 100 or more, then this will subtract 1 game from total games and print their current
+        #  games score for each computer. then it will reset their scores and continue on to the next game.
+        if swear_im_human >= 100 or totes_not_robot >= 100:
             game_total -= 1
             print("totes_not_robot:", totes_not_robot, "swear_im_human:", swear_im_human)
             swear_im_human -= swear_im_human
@@ -121,6 +133,6 @@ def world_championship(games, goal_1, goal_2):
 
 if __name__ == '__main__':
     pass
-    #human_vs_computer()
-    #human_turn(10)
-    #validate_response("Does this work (t/f)?",'t','f')
+    # human_vs_computer()
+    # human_turn(10)
+    # validate_response("Does this work (t/f)?",'t','f')
